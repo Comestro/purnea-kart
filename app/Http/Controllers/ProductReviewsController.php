be2b7c76b77ecaeee3063d7a98c6e04672ed556a
SaritaKumari24 <saritaakumari24@gmail.com>
2024-10-01 17:29:17 +0530

    create table

---
+++ app/Http/Controllers/ProductReviewsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductReviews;
+use Illuminate\Http\Request;
+
+class ProductReviewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductReviews $productReviews)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductReviews $productReviews)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProductReviews $productReviews)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductReviews $productReviews)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_10_01_102745_create_vendors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+            $table->string('store_name');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');  
+            $table->string('company_name');  
+            $table->text('store_description');  
+            $table->string("vendor_slug")->unique();
+            $table->string('store_image')->nullable();  
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
