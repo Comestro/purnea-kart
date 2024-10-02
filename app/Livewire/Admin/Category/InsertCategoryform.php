3671e5f76025a36772606bd1075475fcfdb1fc7c
Saumy Anand <samcool3203@gmail.com>
2024-10-02 20:52:32 +0530

    category added with livewire

--- app/Livewire/Admin/Category/InsertCategoryform.php
+++ app/Livewire/Admin/Category/InsertCategoryform.php
@@ -5,12 +5,14 @@ namespace App\Livewire\Admin\Category;
 use App\Models\Category;
 use Livewire\Component;
 use Illuminate\Support\Str;
+use Livewire\WithFileUploads;
 
 
 class InsertCategoryform extends Component
 {
+    use WithFileUploads;
     public $parent_category_id;
-    public $cat_title;
+    public $cat_title  ;
     public $image;
     public $cat_description;
     public $cat_slug;
@@ -26,8 +28,9 @@ class InsertCategoryform extends Component
         ];
     }
 
-    public function updatedTitle($value)
+    public function updatedCatTitle($value)
     {
+        // dd("testing");
         $this->cat_slug = Str::slug($value);
     }
 
@@ -37,34 +40,34 @@ class InsertCategoryform extends Component
         // Validate inputs based on the rules set in the class
         $validatedData = $this->validate();
 
-        if ($this->photo) {
-            $imageName = "C" . time() . '.' . $this->photo->getClientOriginalExtension();
-            $this->photo->storeAs('public/image/category', $imageName);
+        if ($this->image) {
+            $imageName = "C" . time() . '.' . $this->image->getClientOriginalExtension();
+            $this->image->storePublicly('public/image/category', $imageName,'public/image/category');
         } else {
             $imageName = null;
         }
 
 
-        // Insert the category into the database
+        // // Insert the category into the database
         $category = Category::create([
             'parent_category_id' =>$this->parent_category_id,
             'name' => $this->cat_title,
             'cat_description' => $this->cat_description,
-            'cat_slug' => $this->slug,
+            'cat_slug' => $this->cat_slug,
             'image' => $imageName,
         ]);
 
         // Redirect with success or error message
         if ($category) {
             session()->flash('success', 'Category added successfully.');
-            return redirect()->route('category.index');
+         
         } else {
             session()->flash('error', 'Unable to add category.');
         }
     }
 
     public function render()
-    {
-        return view('livewire.admin.category.insert-categoryform');
+    {  $data['categories'] = Category::where('parent_category_id',NULL)->get();
+        return view('livewire.admin.category.insert-categoryform',$data);
     }
 }
