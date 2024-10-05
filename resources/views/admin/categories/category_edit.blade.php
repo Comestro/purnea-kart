84560e63bff04f031bdcd7c040b674e48dcea1a9
Saumy Anand <samcool3203@gmail.com>
2024-10-05 20:54:28 +0530

    footer removed

--- resources/views/admin/categories/category_edit.blade.php
+++ resources/views/admin/categories/category_edit.blade.php
@@ -254,7 +254,40 @@
 
                     <div class="row">
                          <div class="col-xl-3 col-lg-4">
-                           
+                              <div class="card">
+                                   <div class="card-body">
+                                        <div class="bg-light text-center rounded bg-light">
+                                             <img src="{{asset('assets_admin/images/product/p-1.png')}}" alt="" class="avatar-xxl">
+                                        </div>
+                                        <div class="mt-3">
+                                             <h4>Fashion Men , Women & Kid's</h4>
+                                             <div class="row">
+                                                  <div class="col-lg-4 col-4">
+                                                       <p class="mb-1 mt-2">Created By :</p>
+                                                       <h5 class="mb-0">Seller</h5>
+                                                  </div>
+                                                  <div class="col-lg-4 col-4">
+                                                       <p class="mb-1 mt-2">Stock :</p>
+                                                       <h5 class="mb-0">46233</h5>
+                                                  </div>
+                                                  <div class="col-lg-4 col-4">
+                                                       <p class="mb-1 mt-2">ID :</p>
+                                                       <h5 class="mb-0">FS16276</h5>
+                                                  </div>
+                                             </div>
+                                        </div>
+                                   </div>
+                                   <div class="card-footer border-top">
+                                        <div class="row g-2">
+                                             <div class="col-lg-6">
+                                                  <a href="#!" class="btn btn-outline-secondary w-100">Create Category</a>
+                                             </div>
+                                             <div class="col-lg-6">
+                                                  <a href="#!" class="btn btn-primary w-100">Cancel</a>
+                                             </div>
+                                        </div>
+                                   </div>
+                              </div>
                          </div>
 
                          <div class="col-xl-9 col-lg-8 ">
@@ -362,17 +395,7 @@
                </div>
                <!-- End Container Fluid -->
 
-               <!-- ========== Footer Start ========== -->
-               <footer class="footer">
-                    <div class="container-fluid">
-                         <div class="row">
-                              <div class="col-12 text-center">
-                                   <script>document.write(new Date().getFullYear())</script> &copy; Larkon. Crafted by <iconify-icon icon="iconamoon:heart-duotone" class="fs-18 align-middle text-danger"></iconify-icon> <a href="" class="fw-bold footer-text" target="_blank">Techzaa</a>
-                              </div>
-                         </div>
-                    </div>
-               </footer>
-               <!-- ========== Footer End ========== -->
+            
 
           </div>
           <!-- ==================================================== -->
