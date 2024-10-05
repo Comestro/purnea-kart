f8495bba3b3cc96d2ec5c5c7122002979a932080
saurav itachi <sauravkumar52778@gmail.com>
2024-10-05 20:39:35 +0530

    unwanted data deleted

--- resources/views/livewire/admin/brand/previous-item.blade.php
+++ resources/views/livewire/admin/brand/previous-item.blade.php
@@ -3,25 +3,20 @@
         @php
             $latestItem = $previous->last();
         @endphp
-        <div class="bg-light text-center rounded">
-            <img src="{{ $latestItem->logo ? asset('storage/public/logo/brand/' . $latestItem->logo) : asset('public/logo/brand/default.png') }}"
-                alt="{{ $latestItem->brand_name }} Logo" style="max-width: 100px; max-height: 100px;">
+        <div>
+            <h4>Recent Insert Data</h4>
+            <div class="bg-light text-center rounded">
+                <img src="{{ $latestItem->logo ? asset('storage/public/logo/brand/' . $latestItem->logo) : asset('public/logo/brand/default.png') }}"
+                    alt="{{ $latestItem->brand_name }} Logo" style="max-width: 100px; max-height: 150px;">
+            </div>
         </div>
         <div class="mt-3">
             <h4>{{ $latestItem->brand_name }}</h4>
             <div class="row">
+                <p class="mb-1 mt-2">Description</p>
                 <div class="col-lg-4 col-4">
-                    <p class="mb-1 mt-2">Created By:</p>
-                    <h5 class="mb-0">Seller</h5>
-                </div>
-                <div class="col-lg-4 col-4">
-                    <p class="mb-1 mt-2">Stock:</p>
-                    <h5 class="mb-0"></h5>
-                </div>
-                <div class="col-lg-4 col-4">
-                    <p class="mb-1 mt-2">ID:</p>
-                    <h5 class="mb-0"></h5> 
-                </div>
+                    <h5 class="mb-0">{{$latestItem->brand_description}}</h5>
+                </div>             
             </div>
         </div>
     @else
