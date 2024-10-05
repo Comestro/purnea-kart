33b617010670f132034bd268bb646b097b5abf73
saurav itachi <sauravkumar52778@gmail.com>
2024-10-05 18:57:23 +0530

    previous done

--- app/Livewire/Admin/Brand/PreviousItem.php
+++ app/Livewire/Admin/Brand/PreviousItem.php
@@ -2,12 +2,27 @@
 
 namespace App\Livewire\Admin\Brand;
 
+use App\Models\Brand;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 
 class PreviousItem extends Component
 {
+    use WithFileUploads;
+    public $searchTerm;
+    public $brand_name;
+    public $brand_description;
+    public $brand_slug;
+    public $brand_logo;
+    protected $rules = [
+        "brand_name" => "required|string|max:255",
+        "brand_description" => "required|string|max:255",
+        "brand_slug" => "required|string|max:255",
+        "logo" => "required|string|max:255",
+    ];    
     public function render()
     {
-        return view('livewire.admin.brand.previous-item');
+        $previous = Brand::where('brand_name', 'like', '%' . $this->searchTerm . '%')->orWhere('brand_name', 'like', '%' . $this->searchTerm . '%')->get();
+        return view('livewire.admin.brand.previous-item',['previous' => $previous]);
     }
 }
--- resources/views/livewire/admin/brand/previous-item.blade.php
+++ resources/views/livewire/admin/brand/previous-item.blade.php
@@ -1,22 +1,30 @@
 <div>
-    <div class="bg-light text-center rounded bg-light">
-        <img src="assets/images/product/p-1.png" alt="" class="avatar-xxl">
-    </div>
-    <div class="mt-3">
-        <h4>Fashion Men , Women & Kid's</h4>
-        <div class="row">
-            <div class="col-lg-4 col-4">
-                <p class="mb-1 mt-2">Created By :</p>
-                <h5 class="mb-0">Seller</h5>
-            </div>
-            <div class="col-lg-4 col-4">
-                <p class="mb-1 mt-2">Stock :</p>
-                <h5 class="mb-0">46233</h5>
-            </div>
-            <div class="col-lg-4 col-4">
-                <p class="mb-1 mt-2">ID :</p>
-                <h5 class="mb-0">FS16276</h5>
+    @if ($previous->isNotEmpty())
+        @php
+            $latestItem = $previous->last();
+        @endphp
+        <div class="bg-light text-center rounded">
+            <img src="{{ $latestItem->logo ? asset('storage/public/logo/brand/' . $latestItem->logo) : asset('public/logo/brand/default.png') }}"
+                alt="{{ $latestItem->brand_name }} Logo" style="max-width: 100px; max-height: 100px;">
+        </div>
+        <div class="mt-3">
+            <h4>{{ $latestItem->brand_name }}</h4>
+            <div class="row">
+                <div class="col-lg-4 col-4">
+                    <p class="mb-1 mt-2">Created By:</p>
+                    <h5 class="mb-0">Seller</h5>
+                </div>
+                <div class="col-lg-4 col-4">
+                    <p class="mb-1 mt-2">Stock:</p>
+                    <h5 class="mb-0"></h5>
+                </div>
+                <div class="col-lg-4 col-4">
+                    <p class="mb-1 mt-2">ID:</p>
+                    <h5 class="mb-0"></h5> 
+                </div>
             </div>
         </div>
-    </div>
+    @else
+        <p>No items available.</p>
+    @endif
 </div>
