18a9c9b2dba90d1ddeacc7b5ebcb79e1aa0447d9
rahulxdev0 <rk854327@gmail.com>
2024-10-07 12:47:21 +0530

    category route work

--- routes/web.php
+++ routes/web.php
@@ -68,7 +68,7 @@ Route::get('admin/categories/category-edit/{cat_id}', function ($cat_id) {
 })->name('edit.category');
 Route::get('admin/categories/category-add', function () {
     return view('admin.categories.category_add');
-});
+})->name('create.category');
 
 //Coupon
 Route::get('admin/coupon/coupon-add',function(){
