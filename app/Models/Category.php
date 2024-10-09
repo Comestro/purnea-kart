4fcd640657e0b1ef24978d9c0c21a99ce95d0381
Saumy Anand <samcool3203@gmail.com>
2024-10-09 21:13:28 +0530

    category delete is on working

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,12 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Category extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
+    public function parent():HasOne
+    {
+        return $this->hasOne(Category::class,'id','parent_id');
+    }
     public function children():HasMany
     {
         return $this->hasMany(Category::class,'parent_id');
--- resources/views/livewire/admin/category/manage-categoryform.blade.php
+++ resources/views/livewire/admin/category/manage-categoryform.blade.php
@@ -1,65 +1,65 @@
 <div>
-    <div class="mb-3 position-relative">
-        <div class="input-group">
-            <input type="search"
-                class="form-control rounded-pill ps-5"
-                placeholder="Search here.."
-                wire:model.live="search">
-            <svg xmlns="http://www.w3.org/2000/svg"
-                class="position-absolute top-50 translate-middle-y text-muted"
-                style="left: 15px;"
-                width="20"
-                height="20"
-                viewBox="0 0 48 48">
-                <path d="M 20.5 6 C 12.509634 6 6 12.50964 6 20.5 C 6 28.49036 12.509634 35 20.5 35 C 23.956359 35 27.133709 33.779044 29.628906 31.75 L 39.439453 41.560547 A 1.50015 1.50015 0 1 0 41.560547 39.439453 L 31.75 29.628906 C 33.779044 27.133709 35 23.956357 35 20.5 C 35 12.50964 28.490366 6 20.5 6 z M 20.5 9 C 26.869047 9 32 14.130957 32 20.5 C 32 23.602612 30.776198 26.405717 28.791016 28.470703 A 1.50015 1.50015 0 0 0 28.470703 28.791016 C 26.405717 30.776199 23.602614 32 20.5 32 C 14.130953 32 9 26.869043 9 20.5 C 9 14.130957 14.130953 9 20.5 9 z"></path>
-            </svg>
-        </div>
+<div class="mb-3 position-relative">
+    <div class="input-group">
+        <input type="search"
+               class="form-control rounded-pill ps-5"
+               placeholder="Search here.."
+               wire:model.live="search">
+        <svg xmlns="http://www.w3.org/2000/svg"
+             class="position-absolute top-50 translate-middle-y text-muted"
+             style="left: 15px;"
+             width="20"
+             height="20"
+             viewBox="0 0 48 48">
+            <path d="M 20.5 6 C 12.509634 6 6 12.50964 6 20.5 C 6 28.49036 12.509634 35 20.5 35 C 23.956359 35 27.133709 33.779044 29.628906 31.75 L 39.439453 41.560547 A 1.50015 1.50015 0 1 0 41.560547 39.439453 L 31.75 29.628906 C 33.779044 27.133709 35 23.956357 35 20.5 C 35 12.50964 28.490366 6 20.5 6 z M 20.5 9 C 26.869047 9 32 14.130957 32 20.5 C 32 23.602612 30.776198 26.405717 28.791016 28.470703 A 1.50015 1.50015 0 0 0 28.470703 28.791016 C 26.405717 30.776199 23.602614 32 20.5 32 C 14.130953 32 9 26.869043 9 20.5 C 9 14.130957 14.130953 9 20.5 9 z"></path>
+        </svg>
     </div>
+</div>
 
-    <div class="row">
-        <div class="col-xl-12">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between align-items-center gap-1">
-                    <h4 class="card-title flex-grow-1">All Categories List</h4>
-                    <a href="product-add.html" class="btn btn-sm btn-primary">
-                        Add Category
+<div class="row">
+    <div class="col-xl-12">
+        <div class="card">
+            <div class="card-header d-flex justify-content-between align-items-center gap-1">
+                <h4 class="card-title flex-grow-1">All Categories List</h4>
+                <a href="product-add.html" class="btn btn-sm btn-primary">
+                    Add Category
+                </a>
+                <div class="dropdown">
+                    <a href="#" class="dropdown-toggle btn btn-sm btn-outline-light" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        This Month
                     </a>
-                    <div class="dropdown">
-                        <a href="#" class="dropdown-toggle btn btn-sm btn-outline-light" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            This Month
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-end">
-                            <!-- item-->
-                            <a href="#!" class="dropdown-item">Download</a>
-                            <!-- item-->
-                            <a href="#!" class="dropdown-item">Export</a>
-                            <!-- item-->
-                            <a href="#!" class="dropdown-item">Import</a>
-                        </div>
+                    <div class="dropdown-menu dropdown-menu-end">
+                        <!-- item-->
+                        <a href="#!" class="dropdown-item">Download</a>
+                        <!-- item-->
+                        <a href="#!" class="dropdown-item">Export</a>
+                        <!-- item-->
+                        <a href="#!" class="dropdown-item">Import</a>
                     </div>
                 </div>
-                <div>
-                    <div class="table-responsive">
-                        <table class="table align-middle mb-0 table-hover table-centered">
-                            <thead class="bg-light-subtle">
-                                <tr>
-                                    <th style="width: 20px;">
-                                        <div class="form-check">
-                                            <input type="checkbox" class="form-check-input" id="customCheck1">
-                                            <label class="form-check-label" for="customCheck1"></label>
-                                        </div>
-                                    </th>
-                                    <th>Id</th>
-                                    <th>Category Name</th>
-                                    <th>Category slug</th>
-                                    <th>Category Description</th>
-                                    <th>Category Logo</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($category as $cat)
+            </div>
+            <div>
+                <div class="table-responsive">
+                    <table class="table align-middle mb-0 table-hover table-centered">
+                        <thead class="bg-light-subtle">
+                            <tr>
+                                <th style="width: 20px;">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="customCheck1">
+                                        <label class="form-check-label" for="customCheck1"></label>
+                                    </div>
+                                </th>
+                                <th>Id</th>
+                                <th>Category Name</th>
+                                <th>Category slug</th>
+                                <th>Category Description</th>
+                                <th>Category Logo</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($category as $cat)
                                 <tr>
                                     <td>
                                         <div class="form-check">
@@ -90,50 +90,48 @@
                                                 class="btn btn-soft-primary btn-sm"><iconify-icon
                                                     icon="solar:pen-2-broken"
                                                     class="align-middle fs-18"></iconify-icon></a>
-                                            <form wire:submit="delete($cat->id)" action="">
-                                                <button type="submit" class="btn btn-soft-danger btn-sm"><iconify-icon
-                                                        icon="solar:trash-bin-minimalistic-2-broken"
-                                                        class="align-middle fs-18"></iconify-icon></button>
-                                            </form>
+                                            <a href="#" class="btn btn-soft-danger btn-sm"><iconify-icon
+                                                    icon="solar:trash-bin-minimalistic-2-broken"
+                                                    class="align-middle fs-18"></iconify-icon></a>
                                         </div>
                                     </td>
                                 </tr>
-                                @endforeach
+                            @endforeach
 
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- end table-responsive -->
+                        </tbody>
+                    </table>
                 </div>
-                <!-- Pagination -->
-                <div class="card-footer border-top">
-                    <nav aria-label="Page navigation example">
-                        <ul class="pagination justify-content-end mb-0">
-                            <li class="page-item {{ $category->onFirstPage() ? 'disabled' : '' }}">
-                                <a class="page-link" wire:click="previousPage" wire:loading.attr="disabled"
-                                    tabindex="-1">Previous</a>
-                            </li>
+                <!-- end table-responsive -->
+            </div>
+            <!-- Pagination -->
+            <div class="card-footer border-top">
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-end mb-0">
+                        <li class="page-item {{ $category->onFirstPage() ? 'disabled' : '' }}">
+                            <a class="page-link" wire:click="previousPage" wire:loading.attr="disabled"
+                                tabindex="-1">Previous</a>
+                        </li>
 
-                            @php
+                        @php
                             $currentPage = $category->currentPage();
                             $lastPage = $category->lastPage();
-                            @endphp
+                        @endphp
 
-                            @for ($i = max(1, $currentPage - 1); $i <= min($currentPage + 1, $lastPage); $i++)
-                                <li class="page-item {{ $i == $currentPage ? 'active' : '' }}">
+                        @for ($i = max(1, $currentPage - 1); $i <= min($currentPage + 1, $lastPage); $i++)
+                            <li class="page-item {{ $i == $currentPage ? 'active' : '' }}">
                                 <a class="page-link" wire:click="gotoPage({{ $i }})">{{ $i }}</a>
-                                </li>
-                                @endfor
+                            </li>
+                        @endfor
 
-                                <!-- Next Page Link -->
-                                <li class="page-item {{ $category->hasMorePages() ? '' : 'disabled' }}">
-                                    <a class="page-link" wire:click="nextPage" wire:loading.attr="disabled">Next</a>
-                                </li>
-                        </ul>
-                    </nav>
-                </div>
+                        <!-- Next Page Link -->
+                        <li class="page-item {{ $category->hasMorePages() ? '' : 'disabled' }}">
+                            <a class="page-link" wire:click="nextPage" wire:loading.attr="disabled">Next</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     </div>
+</div>
 
 </div>
