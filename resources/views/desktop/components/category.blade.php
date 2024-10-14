c9774181e303111de37ebc5be374aa24551a9ba0
rahulxdev0 <rk854327@gmail.com>
2024-10-14 13:46:06 +0530

    category design

--- resources/views/desktop/components/category.blade.php
+++ resources/views/desktop/components/category.blade.php
@@ -1,42 +1,42 @@
 <div class="flex w-full justify-around items-center bg-white gap-5 shadow-md px-4 py-3 overflow-hidden">
-    <div class="text-center bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="Top Offers" class="mx-auto rounded-full shadow">
         <p class="mt-2 text-gray-700 font-medium text-xs truncate">Top Offers</p>
     </div>
-    <div class="text-center bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="Mobiles & Tablets" class="mx-auto rounded-full shadow">
-        <p class="mt-2 text-gray-700 font-medium text-xs truncate">Mobiles & Tablets</p>
+        <p class="mt-2 text-gray-700 fontsemiboldm text-xs truncate">Mobiles & Tablets</p>
     </div>
-    <div class="text-center  bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center  p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="TVs & Appliances" class="mx-auto rounded-full shadow">
-        <p class="mt-2 text-gray-700 font-medium text-xs truncate">TVs & Appliances</p>
+        <p class="mt-2 text-gray-700 font-semibold text-xs truncate">TVs & Appliances</p>
     </div>
-    <div class="text-center  bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center  p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="Electronics" class="mx-auto rounded-full shadow">
-        <p class="mt-2 text-gray-700 font-medium text-xs">Electronics</p>
+        <p class="mt-2 text-gray-700 font-semibold text-xs">Electronics</p>
     </div>
-    <div class="text-center  bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center  p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="Fashion" class="mx-auto rounded-full shadow">
-        <p class="mt-2 text-gray-700 font-medium text-xs">Fashion</p>
+        <p class="mt-2 text-gray-700 font-semibold text-xs">Fashion</p>
     </div>
-    <div class="text-center  bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center  p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="Beauty" class="mx-auto rounded-full shadow">
-        <p class="mt-2 text-gray-700 font-medium text-xs">Beauty</p>
+        <p class="mt-2 text-gray-700 font-semibold text-xs">Beauty</p>
     </div>
-    <div class="text-center  bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center  p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="Home & Kitchen" class="mx-auto rounded-full shadow">
-        <p class="mt-2 text-gray-700 font-medium text-xs truncate">Home & Kitchen</p>
+        <p class="mt-2 text-gray-700 font-semibold text-xs truncate">Home & Kitchen</p>
     </div>
-    <div class="text-center  bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center  p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="Furniture" class="mx-auto rounded-full shadow">
-        <p class="mt-2 text-gray-700 font-medium text-xs">Furniture</p>
+        <p class="mt-2 text-gray-700 font-semibold text-xs">Furniture</p>
     </div>
-    <div class="text-center  bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center  p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="Travel" class="mx-auto rounded-full shadow">
-        <p class="mt-2 text-gray-700 font-medium text-xs">Travel</p>
+        <p class="mt-2 text-gray-700 font-semibold text-xs">Travel</p>
     </div>
-    <div class="text-center  bg-red-50 p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
+    <div class="text-center  p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
         <img src="https://picsum.photos/50" alt="Grocery" class="mx-auto rounded-full shadow">
-        <p class="mt-2 text-gray-700 font-medium text-xs">Grocery</p>
+        <p class="mt-2 text-gray-700 font-semibold text-xs">Grocery</p>
     </div>
 </div>
