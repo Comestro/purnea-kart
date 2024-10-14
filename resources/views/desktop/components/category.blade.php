6fb55822b1bebd41b468f23910cc247357d5cc4f
rahulxdev0 <rk854327@gmail.com>
2024-10-14 20:40:05 +0530

    calling product on home page done

--- resources/views/desktop/components/category.blade.php
+++ resources/views/desktop/components/category.blade.php
@@ -2,7 +2,7 @@
     @foreach ($categories as $cat)
         <div class="text-center p-1 rounded-md" style="min-width: 100px; min-height: 85px;">
             <img src="{{ asset('storage/public/image/category/'.$cat->image) }}" alt="Top Offers" class="mx-auto h-[55px]">
-            <p class="mt-2 text-gray-700 font-bold text-xs truncate">{{ $cat->cat_title }}</p>
+            <p class="mt-2 text-gray-700 font-semibold truncate">{{ $cat->cat_title }}</p>
         </div>
     @endforeach
 </div>
