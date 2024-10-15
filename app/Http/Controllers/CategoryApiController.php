ce579e526c2612f07eb50f784248f963536b1d9f
saurav itachi <sauravkumar52778@gmail.com>
2024-10-15 03:40:17 -0700

    delete category api on process

--- app/Http/Controllers/CategoryApiController.php
+++ app/Http/Controllers/CategoryApiController.php
@@ -14,7 +14,12 @@ class CategoryApiController extends Controller
      */
     public function index()
     {
-        //
+       $categories = Category::all();
+       return response()->json([
+        'message' => 'Categories fetched successfully',
+        'categories' => $categories
+    ], 200);
+       
     }
 
     /**
@@ -47,22 +52,49 @@ class CategoryApiController extends Controller
      */
     public function show(Category $category)
     {
-        //
+        if (!$category) {
+            return response()->json([
+                'message' => 'Category not found'
+            ], 404);
+        }
+    
+        return response()->json([
+            'message' => 'Category fetched successfully',
+            'category' => $category
+        ], 200);
     }
+    
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Category $category)
-    {
-        //
+    public function update(StoreCategoryReq $request, Category $category)
+    {       
+        $category->cat_title = $request->cat_title;
+        $category->parent_category_id = $request->parent_category_id;
+        $category->cat_slug = $request->cat_slug;
+        $category->cat_description = $request->cat_description;
+
+        
+        if ($request->hasFile('image')) {
+            $imageName = time() . '.' . $request->image->extension();
+            $request->image->storeAs('public/image/category', $imageName);
+            $category->image = $imageName;
+        }        
+        $category->save();
+       
+        return response()->json([
+            'message' => 'Category updated successfully',
+            'category' => $category
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(Category $category)
-    {
-        //
+    {       
+        return $category->delete()   ?   response()->json(['message' => 'Category deleted successfully'], 200) : response()->json(['message' => 'Category not found or could not be deleted'], 404);
     }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 // POST request to create a category
 Route::post('admin/categories/category-add', [CategoryApiController::class, 'store']);
+// routes/api.php
+Route::get('admin/categories/{category}', [CategoryApiController::class, 'show']);
+Route::put('admin/categories/{category}', [CategoryApiController::class, 'update']);
+Route::delete('admin/categories/{category}', [CategoryApiController::class, 'destroy']);
