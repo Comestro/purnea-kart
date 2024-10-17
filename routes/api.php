93c38d3cbd714abfd109431fe32620a4138afc9c
saurav itachi <sauravkumar52778@gmail.com>
2024-10-17 11:30:39 -0700

    categpry get api done

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\CategoryApiController;
 use Illuminate\Support\Facades\Route;
 
 // POST request to create a category
-Route::post('admin/categories/category-add', [CategoryApiController::class, 'store']);
-// routes/api.php
-Route::get('admin/categories/{category}', [CategoryApiController::class, 'show']);
-Route::put('admin/categories/{category}', [CategoryApiController::class, 'update']);
-Route::delete('admin/categories/{category}', [CategoryApiController::class, 'destroy']);
+Route::get('/categories', [CategoryApiController::class, 'index']);
+
+Route::post('categories/category-add', [CategoryApiController::class, 'store']);
+Route::get('/categories/{category}', [CategoryApiController::class, 'show']);
+Route::put('/categories/{category}', [CategoryApiController::class, 'update']);
+Route::delete('/categories/{category}', [CategoryApiController::class, 'destroy']);
