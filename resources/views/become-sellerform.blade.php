f282c1575c41abb535f30c16a0a97142f0264c83
Saumy Anand <samcool3203@gmail.com>
2024-10-18 19:55:50 +0530

    become seller controller added

---
+++ resources/views/become-sellerform.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>become a seller</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="flex flex-1 justify-center">
+        <form action="" method="post" class="5/12">
+            @csrf
+            <div class="w-full mb-3">
+                <input type="number" class="px-3 py-3 rounded-md border border-slate-300" placeholder="Enter Mobile Number">
+            </div>
+            <div class="w-full mb-3">
+                <input type="number" class="px-3 py-3 rounded-md border border-slate-300" placeholder="Email ID">
+            </div>
+            <div>
+                <input type="text">
+            </div>
+        </form>
+
+    </div>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.14.3/dist/cdn.min.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,12 @@ use App\Models\Product;
 use App\Models\ProductImage;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SellerController;
 use App\Models\Setting;
 use App\Models\Banner;
 use App\Models\Brand;
+
+use function Livewire\store;
 
 Route::get('/', function(){
 
@@ -17,6 +20,8 @@ Route::get('/', function(){
     $data['brands'] = Brand::all();
     return view('home', $data);
 })->name('home');
+
+Route::get('become-seller',[SellerController::class,'index'])->name('seller.register');
 
 Route::get('/view/{product_slug}', function($product_slug){
     $data['product'] = Product::where('slug',$product_slug)->first();
