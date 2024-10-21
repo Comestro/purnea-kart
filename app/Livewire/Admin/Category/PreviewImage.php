f24a0a6acfa6b18b54cffa12501a4da4e5f0fbfe
puja-03 <pujakumari11th2003gmail.com>
2024-10-21 13:46:02 +0530

    small changes

---
+++ app/Livewire/Admin/Category/PreviewImage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Category;
+
+use Livewire\Component;
+
+class PreviewImage extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.category.preview-image');
+    }
+}
