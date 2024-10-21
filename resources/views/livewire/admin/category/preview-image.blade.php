2ddc327005a26301a8fac32769a63ad5f971c70a
puja-03 <pujakumari11th2003gmail.com>
2024-10-21 14:25:50 +0530

    calling preview image

--- resources/views/livewire/admin/category/preview-image.blade.php
+++ resources/views/livewire/admin/category/preview-image.blade.php
@@ -1,46 +1,16 @@
 <div>
     <div class="row">
+        @foreach ($previewimage as $key=>$pvi)
         <div class="col-md-6 col-xl-3">
-             <div class="card">
-                  <div class="card-body text-center">
-                       <div class="rounded bg-secondary-subtle d-flex align-items-center justify-content-center mx-auto">
-                            <img src="{{asset('assets_admin/images/product/p-1.png')}}" alt="" class="avatar-xl">
-                       </div>
-                       <h4 class="mt-3 mb-0">Fashion Categories</h4>
-                  </div>
-             </div>
-        </div>
-        <div class="col-md-6 col-xl-3">
-             <div class="card">
-                  <div class="card-body text-center">
-                       <div class="rounded bg-primary-subtle d-flex align-items-center justify-content-center mx-auto">
-                            <img src="{{asset('assets_admin/images/product/p-6.png')}}" alt="" class="avatar-xl">
-                       </div>
-                       <h4 class="mt-3 mb-0">Electronics Headphone</h4>
-                  </div>
-             </div>
-        </div>
-
-        <div class="col-md-6 col-xl-3">
-             <div class="card">
-                  <div class="card-body text-center">
-                       <div class="rounded bg-warning-subtle d-flex align-items-center justify-content-center mx-auto">
-                            <img src="{{asset('assets_admin/images/product/p-7.png')}}" alt="" class="avatar-xl">
-                       </div>
-                       <h4 class="mt-3 mb-0">Foot Wares</h4>
-                  </div>
-             </div>
-        </div>
-
-        <div class="col-md-6 col-xl-3">
-             <div class="card">
-                  <div class="card-body text-center">
-                       <div class="rounded bg-info-subtle d-flex align-items-center justify-content-center mx-auto">
-                            <img src="{{asset('assets_admin/images/product/p-9.png')}}" alt="" class="avatar-xl">
-                       </div>
-                       <h4 class="mt-3 mb-0">Eye Ware & Sunglass</h4>
-                  </div>
-             </div>
-        </div>
+            <div class="card">
+                 <div class="card-body text-center">
+                      <div class="rounded bg-primary-subtle d-flex align-items-center justify-content-center mx-auto">
+                           <img src="{{ asset('https://ronilaravel.s3.amazonaws.com/public/image/category/'. $pvi->image) }}" alt="" class="avatar-xl">
+                      </div>
+                      <h4 class="mt-3 mb-0">{{$pvi->cat_title}}</h4>
+                 </div>
+            </div>
+       </div>
+       @endforeach
    </div>
 </div>
