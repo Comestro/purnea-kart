64f0d56f3118f1eb6668006f4aa5d0de2045b0de
Archana-PC <archanamikki1998@gmail.com>
2024-10-22 11:15:05 +0530

    api login work

--- purnea-kart-frontend/src/App.jsx
+++ purnea-kart-frontend/src/App.jsx
@@ -5,6 +5,9 @@ import Categories from './components/Categories/Categories'
 import Brand from './components/Brand/Brand'
 import Footer from './components/Footer/Footer'
 import Offer from './components/Offer/Offer'
+import { Provider } from 'react-redux'
+import store from './store/store'
+import { BrowserRouter } from 'react-router-dom'
 
 
 
@@ -12,14 +15,18 @@ import Offer from './components/Offer/Offer'
 
 function App() {
   return (
-    <div className='h-fit bg-slate-100'>
-      <Header/>
-      <Categories/>
-      <Offer/>
-      <Products/>
-      <Brand/>
-      <Footer/>
-    </div>
+   <Provider store={store}>
+     <BrowserRouter>
+       <div className='h-fit bg-slate-100'>
+          <Header/>
+          <Categories/>
+          <Offer/>
+          <Products/>
+          <Brand/>
+          <Footer/>
+       </div>
+     </BrowserRouter>
+   </Provider>
   )
 }
 
--- purnea-kart-frontend/src/components/Button.jsx
+++ purnea-kart-frontend/src/components/Button.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
-function Button() {
+function Button({
+    Children,
+    type = 'button',
+    bgcolor = "bg-blue-600",
+    textColor = "text-white",
+    classname = "",
+    ...props   // this for additional properties to be passed to the button
+}) {
   return (
-    <div>Button</div>
+    <div className={`px-4 py-2 rounded ${bgcolor} ${textColor} ${classname}`}{...props}>{Children}</div>
   )
 }
 
