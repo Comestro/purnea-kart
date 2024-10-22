0d1f6975898e6062736ac2241f400bedf695e1df
khobabe <khobabe007@gmail.com>
2024-10-23 00:02:16 +0530

    signup & sign in work completed

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PublicController extends Controller
 {
@@ -24,6 +27,52 @@ class PublicController extends Controller
 
     public function SustainAbility(){
         return view('about-us.sustainability');
+    }
+
+    // showing signup form here:
+    public function ShowRegisterForm(){
+        return view('account.signup');
+    }
+
+    // signup logics here:
+    public function register(Request $request){
+        $data = $request->validate([
+            'name' => ['required','string','max:255'],
+            'email' => ['required','email','unique:users,email'],
+            'password' => ['required','string','min:8','confirmed'],
+        ]);
+
+        $data['password'] = ($data['password']);
+        
+        $user = User::create($data);
+
+        Auth::login($user);
+
+        return redirect()->intended('/')->with('success','Registration successfull');
+    }
+
+    // showing login form here:
+    public function ShowLoginForm(){
+        return view('account.login');
+    }
+
+    // login logics here:
+    public function Login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required'],
+            'password' => 'required',
+        ]);
+
+        if(Auth::attempt($credentials)){ 
+          
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
     }
     
 }
---
+++ resources/views/account/login.blade.php
@@ -0,0 +1,54 @@
+@extends('layout')
+
+@section('title')
+    Account | Login
+@endsection
+
+<br>
+<br>
+<br>
+<br>
+<br>
+
+@section('content')
+    <div class="flex justify-center items-center px-[1%] py-5 bg-gray-100">
+        <div class="flex bg-white  rounded-lg shadow-lg p-8 text-gray-800 flex-col w-1/3">
+            <h2 class="text-3xl font-bold mb-2 text-center">Login</h2>
+            <p class="text-md text-gray-600 mb-4 text-center">Get access to your Orders, Wishlist, and Recommendations</p>
+
+            <!-- Google Authentication Button -->
+            <div class="flex flex-col items-center mb-4">
+                <a href=""
+                    class="bg-blue-600 text-white rounded p-2 w-full text-center hover:bg-blue-500 transition duration-200 flex justify-center gap-10 items-center">
+                    <img class="h-8 w-8 rounded-full" src="{{ asset('account/download.jpeg') }}" alt="">
+                    Sign in with Google
+                </a>
+
+            </div>
+
+            <form method="POST" action="{{ route('account.login') }}">
+                @csrf
+                <div class="mb-4">
+                    <label for="email" class="block text-gray-700">Email:</label>
+                    <input type="email" id="email" name="email" required
+                        class="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        placeholder="Enter your email">
+                </div>
+                <div class="mb-4">
+                    <label for="password" class="block text-gray-700">Password:</label>
+                    <input type="password" id="password" name="password" required
+                        class="w-full p-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        placeholder="Enter your password">
+                </div>
+                <button type="submit"
+                    class="bg-blue-600 text-white rounded p-3 hover:bg-blue-500 transition duration-200">Login</button>
+            </form>
+            <div class="mt-4 text-center">
+                <p class="text-gray-700">Don't have an account?</p>
+                <a href="{{ route('account.register.form') }}"
+                    class="inline-block mt-2 bg-gray-200 text-gray-700 rounded p-2 hover:bg-gray-300 transition duration-200">Sign
+                    Up</a>
+            </div>
+        </div>
+    </div>
+@endsection
