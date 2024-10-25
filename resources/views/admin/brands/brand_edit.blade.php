d2e8b3031051feb230ce5bf620fa9932d148c633
puja-03 <pujakumari11th2003gmail.com>
2024-10-25 12:04:12 +0530

    Small changes

--- resources/views/admin/brands/brand_edit.blade.php
+++ resources/views/admin/brands/brand_edit.blade.php
@@ -3,35 +3,19 @@
 
 @section('content')
 
-
-
-    <!-- Start right Content here -->
-    <!-- ==================================================== -->
     <div class="page-content">
-
         <!-- Start Container Fluid -->
         <div class="container-xxl">
-
             <div class="row">
-                <div class="col-xl-3 col-lg-4">
+                <div class="col-xl-3 col-lg-4">    
                     <livewire:admin.brand.previous-item />
                 </div>
 
                 <div class="col-xl-9 col-lg-8 ">
                     <livewire:admin.brand.edit-brand :brand='$brand_id' />
-                    
                 </div>
             </div>
         </div>
-
     </div>
-    <!-- End Container Fluid -->
-
-
-
-    </div>
-    <!-- ==================================================== -->
-    <!-- End Page Content -->
-    <!-- ==================================================== -->
 
 @endsection
