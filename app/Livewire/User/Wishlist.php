4581d44d98875d9cd1e5c839194220f0e28f6c7d
saurav itachi <sauravkumar52778@gmail.com>
2024-11-03 06:11:40 -0800

    wishlist categorywish product view

--- app/Livewire/User/Wishlist.php
+++ app/Livewire/User/Wishlist.php
@@ -11,19 +11,39 @@ class Wishlist extends Component
     public $wishlist;
 
     public function mount(){
-        $this->wishlist =ModelsWishlist::where('user_id', Auth::id())->get();
+        $this->wishlist =ModelsWishlist::get();
+        // where('user_id', Auth::id())
     }
 
     public function destroy($id){
         ModelsWishlist::find($id)->delete();
         session()->flash('message','required product removed from your wishlist'); 
-        $this->wishlist = ModelsWishlist::where('user_id',Auth::id())->get(); //to refresh the wishlist after deletion;
+        $this->wishlist = ModelsWishlist::get(); //to refresh the wishlist after deletion;
+        // where('user_id',Auth::id())
     }
+
+
+    public function categories()
+    {
+        $wishlistSet = ModelsWishlist::with('product.category')->get();
+        // ->where('user_id', Auth::id())
+    
+        return $wishlistSet->groupBy(function($item) {
+            return $item->product->category->cat_title ?? "NotCategory";
+        });
+    }
+    
 
 
     public function render()
     {
         $wishlist = $this->wishlist;
-        return view('livewire.user.wishlist', ['wishlist' => $wishlist]);
+        $grouplistwishlist = $this->categories();
+
+        return view('livewire.user.wishlist', [
+            'wishlist' => $this->wishlist,
+            'grouplistwishlist' => $grouplistwishlist 
+        ]);
     }
+    
 }
