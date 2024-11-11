7bffc656c4652497a0e129ae2cc9d21a29b097f5
saurav itachi <sauravkumar52778@gmail.com>
2024-11-11 02:43:01 -0800

    email already error handle

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -88,13 +88,17 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+        if (User::where('email', $request->email)->exists()) {
+            return response()->json(['error' => 'Email already in use.'], 409);
+        }
+
         $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users,email',
             'password' => 'required|string|min:6',
-        ], [
-            'email.unique' => 'The email has already been taken.',
         ]);
+
+
 
         $user = User::create([
             'name' => $validatedData['name'],
